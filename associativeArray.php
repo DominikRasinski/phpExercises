8c25a3289c687ae518e84b7e85ce67d0d5691802
Dominik Rasiński <dox.gmr@gmail.com>
2022-02-05 23:35:05 +0100

    modified associativeArrays

--- associativeArray.php
+++ associativeArray.php
@@ -1,9 +1,17 @@
 <?php
 
+function printAllValueFromAarray($array)
+{
+    foreach($array as $key => $value)
+    {
+        echo $key . ' => ' . $value . '</br>';
+    }
+}
+
 //asocjacyjne tablice wykorzystuja klucze aby dobierac sie do elementow zapisanych w tablicy
 //w uproszczeniu to wyglada to tak key => value 
 
-$people = array("David" => 23, "Amy" => 11);
+$people = array("David" => 23, "Amy" => 11, "Damian" => 1, "Mati" => 20, "Tomek" => 56, "Aloa" => 43);
 
 echo $people['Amy'] . '</br>';
 echo $people['David'] . '</br>';
@@ -21,3 +29,26 @@ foreach($people as $key => $value) //petla przechodzi po tablicy i wybiera z nie
 {
     echo $key . " -> " .  $value . '</br>';
 }
+
+// nastepny sposob na szybkie wyswietlenie tablicy asocjacyjnej polega na wypisaniu jej za pomoca konsktruckji jeszykowej print_r
+
+print_r($people); //w tym przypadku nie mamy zadnej mozliwosci modyfikacji jak sa wyswietlane dane
+echo '</br>';
+
+/**
+ * Sortowanie tablic asocjacyjnych mozna wykonac na dwa sposoby
+ */
+
+//Pierwszy sposob wykorzystanie metody 'asort()' ktora sertuje tablice asoscjacyjna wedlug wartosci
+
+echo "Sortowanie za pomoca metody 'asort()'" . '</br>';
+asort($people);
+printAllValueFromAarray($people);
+echo '#####################################' . '</br>';
+
+//Drugi sposob to sortowanie tablicy asocjacyjnej wedlug kluczy do czego sluzy metoda 'ksort()'
+
+echo "Sortowanie za pomoca metody 'ksort()'" . '</br>';
+ksort($people);
+printAllValueFromAarray($people);
+echo '#####################################' . '</br>';
